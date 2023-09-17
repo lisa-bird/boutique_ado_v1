237f741352f5faeb60991d98b7078b8d9656dd3c
lisa-bird <>
2023-09-17 19:42:53 +0000

    deployment part 1

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -27,7 +27,11 @@ SECRET_KEY = 'django-insecure-b$0583+=cw%)oglfw)0gcevfmi0ag+c)p7p^hj!j!ry!1@gax+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-lisa-bird-boutique-ado-v-mmq5gfm7jw.us2.codeanyapp.com']
+ALLOWED_HOSTS = [
+                '8000-lisa-bird-boutique-ado-v-mmq5gfm7jw.us2.codeanyapp.com',
+                'boutique-ado-lb',
+                'localhost',
+]
 
 
 # Application definition
@@ -121,12 +125,17 @@ WSGI_APPLICATION = 'boutique_ado.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
